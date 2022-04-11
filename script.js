8d643ab364ea7d39f385252589a517baea1e6320
Victor Hugo Meurer <meurervictorhugo@gmail.com>
2022-04-11 02:03:30 -0300

    Fim de jogo implementado

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let numberCards, firstClick;
+let numberCards, firstClick, firstCard, plays = 0, secondCard;
 
 function gameStart(){
     alert ("Esse é o jogo da memória dos Papagaios, por favor, escolha um número entre 4 e 14 cartas, lembrando de escolher sempre números pares e escrever em caracteres numéricos");
@@ -24,7 +24,7 @@ function dealCards(numberCards){
         for (let i = 0; i < cardsInGame.length; i ++){
             list.innerHTML += `
             <li>
-                <div class = "parrotCard" onclick="cardFlip(this)">
+                <div class = "parrotCard" onclick="selectCard(this)">
                     <div class = "front-face face">
                         <img src = "./img/front.png">
                     </div>
@@ -44,9 +44,45 @@ function ulSizeAdjust(list, numberCards){
     list.style.width = `${132 * (numberCards/2)}px`
 }
 
+function selectCard(element){
+    const click = element.children[1].innerHTML;
+    if (firstClick === undefined) {
+        cardFlip(element);
+        firstClick = click;
+        firstCard = element;
+        plays ++;
+    } else if (click === firstClick) {
+        cardFlip(element);
+        firstClick = undefined;
+        plays ++;
+        endgame();
+    } else if (click !== firstClick) {
+        cardFlip(element);
+        secondCard = element;
+        setTimeout(cardUnflip, 1000);
+        firstClick = undefined;
+        plays ++;
+    }
+}
+
 function cardFlip(card){
     card.children[0].classList.add("flipped");
     card.children[1].classList.add("flipped");
 }
 
-gameStart()
+function cardUnflip(){
+    firstCard.children[0].classList.remove("flipped");
+    firstCard.children[1].classList.remove("flipped");
+    secondCard.children[0].classList.remove("flipped");
+    secondCard.children[1].classList.remove("flipped");
+}
+
+function endgame(){
+    const cards = document.querySelectorAll("div.flipped");
+
+    if (cards.length === 2*numberCards){
+        alert("Você ganhou em " + plays + " jogadas!");
+    }
+}
+
+gameStart();
